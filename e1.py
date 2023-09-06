69b43a8b39fb4674266da565ac10c15cd821ab71
Debacle2 <143205054+Debacle2@users.noreply.github.com>
2023-09-06 10:59:12 -0500

    Update e1.py

--- e1.py
+++ e1.py
@@ -1,3 +1,27 @@
-a1v1 = ["Red", "Green", "Blue", "Yellow"]
-for x in a1v1:
-    print(x)
+def e1m1(em1):
+    print (str(em1))
+
+def e1m2():
+    return input("Enter Input: ")
+#    return em2
+
+def e1m3(em1,em2):
+    print(int(em1) + int(em2))
+
+def e1m4(em1,em2,em3):
+    em4 = int(em1) + int(em2) + int(em3)
+    return em4
+
+def e1m5(em1,em2):
+    if (em1 < em2):
+        print (str(em2))
+    else:
+        print (str(em1))
+
+e1m1(e1m2())
+
+e1m3(e1m2(),e1m2())
+
+e1m1(e1m4(e1m2(), e1m2(), e1m2()))
+
+e1m5(e1m2(), e1m2())
